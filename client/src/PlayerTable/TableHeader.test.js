af872a0478f272e0c49cef1b2f585b12a5fb4ecf
RachelKoldenhoven <rachel@koldenfrozen.com>
2019-05-27 11:56:45 -0600

    failing test for Player Add btn

--- client/src/PlayerTable/TableHeader.test.js
+++ client/src/PlayerTable/TableHeader.test.js
@@ -7,9 +7,11 @@ describe('TableHeader', () => {
   it('should have an Add Player button', () => {
     // Setup
     const thWrapper = shallow(<TableHeader />);
+    const addPlayerBtn = thWrapper.find('.table__player-add');
 
     // Exercise
 
     // Assert
+    expect(addPlayerBtn).toHaveLength(1);
   });
 });
